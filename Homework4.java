031c44b84146da94a7fff588b90ebbee5cfdfaa9
kimgun123456789 <127281472+kimgun123456789@users.noreply.github.com>
2025-10-14 14:29:30 +0900

    Create Homework4.java

---
+++ Homework4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class GCD {
+
+        int makeGCD(int m,int n){
+            if (n == 0) {
+                return m;
+            }
+            else {
+                return makeGCD(n, m % n);}
+        }
+    }
+public class Homework4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("두개의 정수 입력:");
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int a;
+        GCD gcd=new GCD();
+        if (m>n){
+            a=gcd.makeGCD(m,n);}
+
+        else if (m==n){
+            a=m;
+        }
+        else{
+            a=gcd.makeGCD(n,m);
+        }
+        System.out.printf("%d",a);
+
+    }
+}
